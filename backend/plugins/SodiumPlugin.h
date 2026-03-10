47dd754d900679ae2c14a41896ff80af2e4c9205
Perry Huang <perryhuang2015@gmail.com>
2026-03-10 18:42:44 -0400

    style: lint plugins

--- backend/plugins/SodiumPlugin.h
+++ backend/plugins/SodiumPlugin.h
@@ -2,22 +2,23 @@
 
 #include <drogon/plugins/Plugin.h>
 #include <sodium.h>
+
 #include <string>
 
-class SodiumPlugin : public drogon::Plugin<SodiumPlugin>
-{
-  public:
-    SodiumPlugin() {}
-    
+class SodiumPlugin : public drogon::Plugin<SodiumPlugin> {
+public:
+    SodiumPlugin() {
+    }
+
     // Standard Drogon Plugin Lifecycle
-    void initAndStart(const Json::Value &config) override;
+    void initAndStart(const Json::Value& config) override;
     void shutdown() override;
 
     // Hashing Logic
-    std::string hashPassword(const std::string &password) const;
-    bool verifyPassword(const std::string &password, const std::string &hash) const;
+    std::string hashPassword(const std::string& password) const;
+    bool verifyPassword(const std::string& password, const std::string& hash) const;
 
-  private:
+private:
     // You can pull these from the config file later if needed
     unsigned long long opsLimit_ = crypto_pwhash_OPSLIMIT_INTERACTIVE;
     size_t memLimit_ = crypto_pwhash_MEMLIMIT_INTERACTIVE;
