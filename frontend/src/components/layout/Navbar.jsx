0db79ea51b63383d9a488938a81d477e07280280
RemiUy05 <remi.uy@nyu.edu>
2026-04-08 14:06:29 -0400

    navbar

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,25 +1,46 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import logoImg from "../../assets/biteful_logo.png"; 
+import "./Navbar.css"; 
 
 const Navbar = ({ isAuthenticated, onLogout }) => {
+    const location = useLocation();
+
     return (
         <nav className="navbar">
-            <div className="nav-logo">
-                <Link to="/">DrogonApp</Link>
+            {/* left: logo + name */}
+            <div className="nav-group left">
+                <div className="logo-container">
+                    <img src={logoImg} alt="Biteful Logo" className="navbar-logo-img" />
+                </div>
+                <Link to="/" className="brand-name">BITEFUL</Link>
             </div>
-            <div className="nav-links">
+
+            {/* middle: tabs */}
+            <div className="nav-group center">
+                {isAuthenticated && (
+                    <>
+                        <Link 
+                            to="/dashboard" 
+                            className={`nav-link ${location.pathname === '/dashboard' ? 'active-red' : ''}`}
+                        >
+                            DASHBOARD
+                        </Link>
+                        <Link 
+                            to="/map/pantry" 
+                            className={`nav-link ${location.pathname === '/map/pantry' ? 'active-red' : ''}`}
+                        >
+                            PANTRY
+                        </Link>
+                    </>
+                )}
+            </div>
+
+            {/* right: logout */}
+            <div className="nav-group right">
                 {isAuthenticated ? (
-                    <>
-                        <Link to="/dashboard">Dashboard</Link>
-                        <Link to="/map/pantry"> Pantry </Link>
-                        <button onClick={onLogout} className="logout-btn">
-                            Logout
-                        </button>
-                    </>
+                    <button onClick={onLogout} className="logout-btn">LOGOUT</button>
                 ) : (
-                    <>
-                        <Link to="/login">Login</Link>
-                        <Link to="/signup">Signup</Link>
-                    </>
+                    <Link to="/login" className="nav-link">Login</Link>
                 )}
             </div>
         </nav>
