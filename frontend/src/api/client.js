b3add7d238b28cddd61da13a83713db440130ae6
niljson <8333jlin@gmail.com>
2026-04-21 21:48:41 -0400

    remove annoying logs

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -10,12 +10,7 @@ export const drogonClient = async (endpoint, options = {}) => {
         headers,
     };
 
-    console.group(`Requesting: ${endpoint}`);
-    console.log("Options:", config);
-    console.groupEnd();
-
     try {
-        // clears cookie
         const response = await fetch(
             import.meta.env.VITE_API_BASE_URL + endpoint,
             config,
@@ -30,19 +25,10 @@ export const drogonClient = async (endpoint, options = {}) => {
 
         // handle json vs non json
         const contentType = response.headers.get("content-type");
-        for (const val of response.headers) {
-            console.log(val);
-        }
         if (contentType && contentType.includes("application/json")) {
-            const resp = await response.json();
-            console.log("json resp");
-            console.log(resp);
-            return resp;
+            return await response.json();
         } else {
-            const resp = await response.text();
-            console.log("test resp");
-            console.log(resp);
-            return resp;
+            return await response.text();
         }
     } catch (error) {
         console.error("Fetch Check:", error);
